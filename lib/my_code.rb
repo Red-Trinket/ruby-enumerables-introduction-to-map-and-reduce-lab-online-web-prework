ef14d05f561e4c9949250d2f77f2302f855b2ce4
Matthew Jones <matts3371@gmail.com>
2019-09-06 03:22:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,3 +3,14 @@ def map_to_negativize(ary)
   ary.map {|n| n * -1}
 end
 
+def map_to_no_change(ary)
+  ary.map {|n| n}
+end
+
+def map_to_double(ary)
+  ary.map {|n| n * 2}
+end
+
+def map_to_square(ary)
+  ary.map {|n| n ** 2}
+end  
